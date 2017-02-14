a4b37f50e6cd1a24815d8b52a441ffcae17a6655
a.gilievyi <a.gilievyi@@terrasoft.ru>
2017-02-14 11:47:18 +0200

    Attribute

---
+++ TerrasoftProfile/TerrasoftProfile.Tests/AttributeTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TerrasoftProfile.Tests
+{
+	using FluentAssertions;
+
+	[TestClass]
+	public class AttributeTests
+	{
+
+		[TestMethod]
+		public void AttributeTest() {
+			var c = new MyCustomClassWithAttribute();
+			var attribute = MyCustomAttributeHelper.GetAttributeObject<MyCustomAttribute>(c.GetType());
+			attribute.Should().NotBeNull();
+			attribute.Description.ShouldBeEquivalentTo("AttributeName");
+		}
+
+		[TestMethod]
+		public void MyCustomAttribute_ctor() {
+			var controlValue = "controlValue";
+			var mca =new MyCustomAttribute(controlValue);
+			mca.Description.ShouldBeEquivalentTo(controlValue);
+		}
+
+		[TestMethod]
+		public void MyCustomAttribute_ctor_null() {
+			var mca = new MyCustomAttribute(null);
+			mca.Description.ShouldBeEquivalentTo(null);
+		}
+
+		[TestMethod]
+		public void MyCustomAttributeHelper_GetAttributeObject_ShouldBeNotNull() {
+			var attribureObject = MyCustomAttributeHelper.GetAttributeObject<MyCustomAttribute>(typeof(MyCustomClassWithAttribute));
+			attribureObject.Should().NotBeNull();
+		}
+
+		[TestMethod]
+		public void MyCustomAttributeHelper_GetAttributeObject_ShouldBeNotNull_Interface() {
+			var attribureObject = MyCustomAttributeHelper.GetAttributeObject<MyCustomAttribute>(typeof(IMyCustomInterfaceWithAttribute));
+			attribureObject.Should().NotBeNull();
+		}
+
+		[TestMethod]
+		public void MyCustomAttributeHelper_GetAttributeObject_ShouldBeNull() {
+			var attribureObject = MyCustomAttributeHelper.GetAttributeObject<MyCustomAttribute>(typeof(MyCustomClassWithoutAttribute));
+			attribureObject.Should().BeNull();
+		}
+
+	}
+}
---
+++ TerrasoftProfile/TerrasoftProfile/Attribute.cs
@@ -0,0 +1,49 @@
+﻿namespace TerrasoftProfile
+{
+	using System;
+
+	[AttributeUsage(AttributeTargets.Class|AttributeTargets.Interface)]
+	public class MyCustomAttribute : Attribute
+	{
+		#region Constructors: Public
+
+		public MyCustomAttribute(string description) {
+			Description = description;
+		}
+
+		#endregion
+
+		#region Properties: Public
+
+		public string Description { get; set; }
+
+		#endregion
+	}
+
+	public static class MyCustomAttributeHelper
+	{
+		public static T GetAttributeObject<T>(Type objectType) where T : class {
+			var attribute = Attribute.GetCustomAttribute(objectType, typeof(T)) as T;
+			return attribute;
+		}
+	}
+
+
+	[MyCustom("AttributeName")]
+	public class MyCustomClassWithAttribute
+	{
+
+	}
+
+	[MyCustom("AttributeName")]
+	public interface IMyCustomInterfaceWithAttribute
+	{
+
+	}
+
+	public class MyCustomClassWithoutAttribute
+	{
+
+	}
+
+}
